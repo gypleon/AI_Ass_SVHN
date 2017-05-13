565f833c3a170a0f9a2fd1ad58d36d084a8af133
Ubuntu <ubuntu@ip-172-31-4-31.us-west-2.compute.internal>
2017-05-13 14:26:00 +0000

    [UPDATE] constructed dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,10 +1,13 @@
+from __future__ import print_function
 import scipy.io as sio
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
 
 # configuration
-BATCH_SIZE = 10
+BATCH_SIZE = 50
+NUM_SUBPLOT_COLS = 10
 DATASET_PATH = "./data/train_32x32.mat"
 
 class DataLoader:
@@ -13,11 +16,17 @@ class DataLoader:
     self.images = data['X']
     self.labels = data['y']
 
-    image_batches = np.reshape()
-    label_batches = []
+    num_samples = self.labels.shape[0]
+    num_batches = num_samples // batch_size
+    # last_batch_size = num_samples - num_batches * batch_size
+    split_conf = [batch_size*(i+1) for i in range(num_batches)]
+    split_conf.append(num_samples)
+
+    self.image_batches = np.split(np.transpose(self.images, (3, 0, 1, 2)), split_conf, axis=0)
+    self.label_batches = np.split(self.labels, split_conf, axis=0)
 
   def iter(self):
-    for image, label in zip(image_batches, label_batches):
+    for image, label in zip(self.image_batches, self.label_batches):
       yield image, label
 
 
@@ -27,12 +36,17 @@ if __name__ == "__main__":
 
   batch_count = 0
   fig = plt.figure()
-  for image, label in dataloader.iter():
+  num_plot_cols = NUM_SUBPLOT_COLS
+  num_plot_rows = int(math.ceil(BATCH_SIZE/num_plot_cols))
+  labels = []
+  for image_batch, label_batch in dataloader.iter():
     if batch_count > 0:
       break
     for batch_i in range(BATCH_SIZE):
-      sub_plot = fig.add_subplot(1, batch_i+1, 1)
-      plt.imshow(image)
+      sub_plot = fig.add_subplot(num_plot_rows, num_plot_cols, batch_i+1)
+      plt.imshow(image_batch[batch_i])
     batch_count += 1
+    labels.append([label[0] for label in label_batch])
 
+  print(labels)
   plt.show()
