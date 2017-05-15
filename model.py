77d3027e680fafedd07be9f6d2264833f67e6e80
Ubuntu <ubuntu@ip-172-31-4-31.us-west-2.compute.internal>
2017-05-15 09:16:45 +0000

    [BACKUP] constructing models

--- model.py
+++ model.py
@@ -2,10 +2,75 @@ import tensorflow as tf
 import numpy as np
 import math
 
-def conv():
+# define model
+''' 
+  input:    [batch, in_height, in_width, in_channels]
+  filter:   [filter_height, filter_width, in_channels, out_channels]
+  padding:  "VALID" - no padding
+            "SAME" - zero padding
+'''
+def conv2d(input_data, num_out_channels, width, height, stride=1, padding="SAME", activation=tf.nn.relu, scope="conv2d"):
+  with tf.variable_scope(scope):
+    weights = tf.get_variable("w", [height, width, input_data.get_shape()[-1], num_out_channels])
+    bias = tf.get_variable("b", [num_out_channels])
+    if activation:
+      return activation(tf.nn.bias_add(tf.nn.conv2d(input_data, weights, strides=[1, stride, stride, 1], padding=padding), bias))
+    else:
+      return tf.nn.bias_add(tf.nn.conv2d(input_data, weights, strides=[1, stride, stride, 1], padding=padding), bias)
 
-def fc():
+''' 
+  input:    [batch, in_channels]
+'''
+def fc(input_data, num_neurons, activation=tf.nn.relu, scope="fc"):
+  shape = input_data.get_shape().as_list()
+  with tf.variable_scope(scope):
+    if len(shape) == 2:
+      weights = tf.get_variable("w", [shape[1], num_neurons])
+      input_data = tf.reshape(input_data, [-1, weights.get_shape().as_list()[0])
+    elif len(shape) == 4:
+      weights = tf.get_variable("w", [shape[1]*shape[2]*shape[3], num_neurons])
+    else:
+      raise ValueError("Linear expects 2D/4D shape: %d" % len(shape))
+    bias = tf.get_variable("b", [num_neurons])
+  if activation:
+    return activation(tf.nn.bias_add(tf.matmul(input_data, weights), bias))
+  else:
+    return tf.nn.bias_add(tf.matmul(input_data, weights), bias)
 
-def max_pool():
+''' 
+  input:    [batch, in_channels]
+'''
+def loss(logits, batch_size, scope="loss"):
+  with tf.variable_scope(scope):
+    labels = tf.placeholder(tf.int64, shape=[batch_size], name="labels")
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits), name="loss")
+  return loss
 
-def avg_pool():
+def optimize(loss, learning_rate=1.0, scope="optimize"):
+  # for continuing training
+  global_step = tf.Variable(0, name='global_step', trainable=False)
+  with tf.variable_scope(scope):
+    learning_rate = tf.get_variable(learning_rate, trainable=False, name='learning_rate')
+    tvars = tf.trainable_variables()
+    grads, global_norm = tf.clip_by_global_norm(tf.gradients(loss, tvars), max_grad_norm)
+    # TODO: other optimizers?
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=global_step)
+  return 
+
+def SVHN(inputs, batch_size, scope="SVHN"):
+  inputs = tf.placeholder(tf.int64, shape=[batch_size, 32, 32], name="input")
+  # convolution section 1
+  conv1 = conv2d()
+  pool = tf.nn.max_pool()
+  # fully connected section 1
+  logits = fc()
+  # loss section
+  loss = loss(logits)
+  
+class SVHN:
+  def __init__(self):
+
+  def inference(self):
+
+  def optimize(self):
--- train.py
+++ train.py
@@ -4,82 +4,30 @@ import time
 import math
 
 from dataloader import DataLoader
+from model import SVHN
 
 # configuration
 BATCH_SIZE = 50
 TRAIN_SET_PATH = "./data/train_32x32.mat"
 MAX_EPOCHS = 100
-LEARNING_RATE = 0.01
+LEARNING_RATE = 1.0
 
-# define model
-''' 
-  input:    [batch, in_height, in_width, in_channels]
-  filter:   [filter_height, filter_width, in_channels, out_channels]
-  strides:  [1, stride_h, stride_w, 1]
-  padding:  "VALID" - no padding
-            "SAME" - zero padding
-'''
-def conv2d(input_data, num_out_channels, width, height, strides, padding, scope):
-  with tf.variable_scope(scope):
-    weights = tf.get_variable("w", [height, width, input_data.get_shape()[-1], num_out_channels])
-    bias = tf.get_variable("b", [num_out_channels])
-    # TODO: activation function?
-  return tf.nn.bias_add(tf.nn.conv2d(input_data, weights, strides=strides, padding=padding), bias)
-
-# TODO: computation
-def fc(input_data, num_neurons, scope):
-  shape = intput_data.get_shape().as_list()
-  dim = 1
-  for d in shape[1:]:
-    dim *= d
-  input_data = tf.reshape(input_data, [-1, dim])
-  with tf.variable_scope(scope):
-    weights = tf.get_variable("w", [input_data.get_shape()[-1]])
-    bias = tf.get_variable("b", [num_neurons])
-  # TODO: activation function?
-  return tf.nn.bias_add(tf.matmul(input_data, weights), bias)
-
-def loss(input_data, scope):
-  with tf.variable_scope(scope):
-    
-  return 
-
-def update(loss, ):
-  with tf.variable_scope(scope):
-    learning_rate = tf.get_variable(learning_rate, trainable=False, name='learning_rate')
-    tvars = tf.trainable_variables()
-    '''
-    grads, global_norm = tf.clip_by_global_norm(tf.gradients(loss, tvars), max_grad_norm)
-    '''
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=global_step)
-  return 
-
-def SVHN():
-  # convolution section 1
-  conv = conv2d()
-  pool = tf.nn.max_pool()
-  # fully connected section 1
-  logits = fc()
-  # loss section
-  loss = loss(logits)
-  # training section
-  update(loss, learning_rate)  
-  
 
 # train model
 def main(_):
+  seed = int(time.time())
+
   train_set_loader = DataLoader(TRAIN_SET_PATH, BATCH_SIZE)
   valid_set_loader = DataLoader(VALID_SET_PATH)
   
   with tf.Graph().as_default(), tf.Session() as session:
-    tf.set_random_seed(time.time())
-    initializer = tf.contrib.layers.xavier_initializer_conv2d(seed=)
+    tf.set_random_seed(seed)
+    initializer = tf.contrib.layers.xavier_initializer_conv2d(seed=seed)
     with tf.variable_scope("SVHN", initializer=initializer):
-      train_model = SVHN()
+      train_model = model.SVHN()
     saver = tf.train.Saver()
     with tf.variable_scope("SVHN", reuse=True):
-      valid_model = SVHN()
+      valid_model = model.SVHN()
     
     tf.global_vairables_initializer().run()
     
