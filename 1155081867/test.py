37e3556726bd320a31a3fbf374620012af24f03c
Ubuntu <ubuntu@ip-172-31-4-31.us-west-2.compute.internal>
2017-05-17 22:11:17 +0000

    [UPDATE] test test

--- 1155081867/test.py
+++ 1155081867/test.py
@@ -15,17 +15,17 @@ import model
 FLAGS = tf.app.flags.FLAGS
 
 # configuration
-tf.app.flags.DEFINE_integer("batch_size", 100, "batch size")
+tf.app.flags.DEFINE_integer("batch_size", 128, "batch size")
 tf.app.flags.DEFINE_integer("valid_batch_size", 1000, "validation batch size")
 tf.app.flags.DEFINE_integer("log_frequency", 10, "log frequency")
 tf.app.flags.DEFINE_integer("eval_frequency", 100, "log frequency")
-tf.app.flags.DEFINE_string ("train_set_path", "./data/train_32x32.mat", "path of the train set")
-tf.app.flags.DEFINE_string ("valid_set_path", "./data/test_32x32.mat", "path of the test set")
-tf.app.flags.DEFINE_string ("log_dir", "/tmp/svhn/logs", "path of checkpoints/logs")
+tf.app.flags.DEFINE_string ("train_set_path", "../data/train_32x32.mat", "path of the train set")
+tf.app.flags.DEFINE_string ("valid_set_path", "../data/test_images.mat", "path of the test set")
+tf.app.flags.DEFINE_string ("log_dir", "./trained_model", "path of checkpoints/logs")
 tf.app.flags.DEFINE_integer("max_steps", 1000000, "max number of steps (batchs)")
-tf.app.flags.DEFINE_float  ("learning_rate", 1.0, "initial learning rate")
+tf.app.flags.DEFINE_float  ("learning_rate", 0.01, "initial learning rate")
 tf.app.flags.DEFINE_boolean('log_device_placement', False, "Whether to log device placement.")
-tf.app.flags.DEFINE_integer('num_examples', 1000, "")
+tf.app.flags.DEFINE_integer('num_valid_examples', 1000, "")
 
 
 # train model
@@ -37,33 +37,45 @@ def main(_):
   seed = int(time.time())
 
   with tf.Graph().as_default():
-    train_loader = DataLoader(FLAGS.train_set_path, FLAGS.batch_size)
-    valid_loader = DataLoader(FLAGS.valid_set_path, FLAGS.valid_batch_size, 1000)
-    train_images, train_labels = train_loader.load_batch()
+    # train_loader = DataLoader(FLAGS.train_set_path, FLAGS.batch_size)
+    valid_loader = DataLoader(FLAGS.valid_set_path, num_valid_samples=1000)
+    # train_images, train_labels = train_loader.load_batch()
     valid_images, valid_labels = valid_loader.load_batch()
 
-    tf.set_random_seed(seed)
-    initializer = tf.contrib.layers.xavier_initializer_conv2d(seed=seed)
-    global_step = tf.contrib.framework.get_or_create_global_step()
+    variable_averages = tf.train.ExponentialMovingAverage(model.MOVING_AVERAGE_DECAY)
+    variables_to_restore = variable_averages.variables_to_restore()
+    saver = tf.train.Saver(variables_to_restore)
 
-    with tf.variable_scope("svhn", initializer=initializer):
-      logits = model.inference(train_images)
-      # logits = tf.Print(logits, [logits], message="TEST logits:", summarize=FLAGS.batch_size)
-      # train_labels = tf.Print(train_labels, [train_labels], message="TEST labels:", summarize=FLAGS.batch_size)
-      loss = model.loss(logits, train_labels)
-      # loss = tf.Print(loss, [loss], message="TEST loss:", summarize=FLAGS.batch_size)
-      train_op = model.optimize(loss, global_step)
+    tf.set_random_seed(seed)
+    # initializer = tf.contrib.layers.xavier_initializer_conv2d(seed=seed)
+    # global_step = tf.contrib.framework.get_or_create_global_step()
+
+    # with tf.variable_scope("svhn", initializer=initializer):
+    #   logits = model.inference(train_images)
+    #   # logits = tf.Print(logits, [logits], message="TEST logits:", summarize=FLAGS.batch_size)
+    #   # train_labels = tf.Print(train_labels, [train_labels], message="TEST labels:", summarize=FLAGS.batch_size)
+    #   loss = model.loss(logits, train_labels)
+    #   # loss = tf.Print(loss, [loss], message="TEST loss:", summarize=FLAGS.batch_size)
+    #   train_op = model.optimize(loss, global_step, FLAGS.learning_rate, FLAGS.batch_size)
 
     with tf.variable_scope("svhn", reuse=True):
       logits = model.inference(valid_images)
       top_k_op = tf.nn.in_top_k(logits, valid_labels, 1)
     
-    scaffold = tf.train.Scaffold(init_op=tf.global_variables_initializer())
+    # scaffold = tf.train.Scaffold(init_op=tf.global_variables_initializer())
+    # saver = tf.train.Saver(tf.trainable_variables())
 
     class _LoggerHook(tf.train.SessionRunHook):
       """Logs loss and runtime."""
       def after_create_session(self, session, coord):
-        train_loader.load(session)
+        # train_loader.load(session)
+        ckpt = tf.train.get_checkpoint_state(FLAGS.log_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+          saver.restore(session, ckpt.model_checkpoint_path)
+          global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        else:
+          print('No checkpoint file found')
+          return
         valid_loader.load(session)
         print("TEST after_create_session")
 
@@ -74,7 +86,7 @@ def main(_):
 
       def end(self, session):
         print("TEST end")
-        train_loader.close(session)
+        # train_loader.close(session)
         valid_loader.close(session)
 
       def before_run(self, run_context):
@@ -94,7 +106,8 @@ def main(_):
           sec_per_batch = float(duration / FLAGS.log_frequency)
 
           if self._step % FLAGS.eval_frequency == 0:
-            precision = model.evaluate(run_context.session, top_k_op, FLAGS.num_examples, FLAGS.batch_size)
+            # TODO: output label
+            precision = model.evaluate(run_context.session, top_k_op, FLAGS.num_valid_examples)
             format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
                           'sec/batch), precision = %.2f%%')
             print(format_str % (datetime.now(), self._step, loss_value,
@@ -107,11 +120,12 @@ def main(_):
 
 
     with tf.train.MonitoredTrainingSession(
-        scaffold=scaffold,
+        # scaffold=scaffold,
         checkpoint_dir=FLAGS.log_dir,
         hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
                tf.train.NanTensorHook(loss),
-               _LoggerHook()],
+               _LoggerHook(),
+               tf.train.CheckpointSaverHook(checkpoint_dir=FLAGS.log_dir, saver=saver, save_steps=100)],
         config=tf.ConfigProto(
             log_device_placement=FLAGS.log_device_placement)) as mon_sess:
       while not mon_sess.should_stop():
